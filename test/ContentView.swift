1d57e0878c4f8e9dfea35e028f17f905bf1ce94b
sami616 <samuelresua@googlemail.com>
2020-08-22 23:04:56 +0100

    Initial Commit

---
+++ test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by Samuel Resua on 22/08/2020.
+//  Copyright © 2020 Samuel Resua. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
